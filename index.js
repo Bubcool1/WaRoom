809f68bba2794eafd255e999c8ca7173d949d636
Oliver Beardsall <oli@beardsall.dev>
2020-03-05 11:24:39 +0000

    Changed names im moron.

--- index.js
+++ index.js
@@ -58,17 +58,15 @@ o League Of Legends (LOL) - :crystal_ball:
 //if this works imma just go die.
 bot.on('message', msg => {
   if (msg.content === "(Please note that not all games have categories. Those that do are marked with a 'o'  for a bullet point)") {
-    msg.react(':chestnut:'))
-    msg.react(':crown:'))
-    msg.react(':map:'))
-    msg.react(':euro:'))
-    msg.react(':tophat:'))
-    msg.react(':star:'))
-    msg.react(':fire:'))
-    msg.react(':airplane:'))
-    msg.react(':crystal_ball:'))
+    msg.react('🌰'))
+    msg.react('👑'))
+    msg.react('🗺️'))
+    msg.react('💶'))
+    msg.react('🎩'))
+    msg.react('⭐'))
+    msg.react('🔥'))
+    msg.react('✈️'))
   }
-
 });
 
 
