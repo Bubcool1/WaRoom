e5d0015e68634ccb1f1fa0957cf6cc4b793c5b42
Oliver Beardsall <oli@beardsall.dev>
2020-03-29 21:46:08 +0100

    dw about it

--- index.js
+++ index.js
@@ -280,8 +280,6 @@ var rolethings=["Führer"];
 client.on('message', msg => {
   if (msg.content === '??Bruh') {
     msg.delete(1)
-    let role = reaction.message.guild.roles.find(r => r.name == rolethings[n]);
-    reaction.message.guild.member(175289853368336394).removeRole(role).catch(console.error);
     let bruhrole = reaction.message.guild.roles.find(r => r.name == rolethings[n]);
     reaction.message.guild.member(182882562526412800).addRole(bruhrole).catch(console.error);
   }
